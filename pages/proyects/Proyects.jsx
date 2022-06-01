2e3ea3aff0d8feb7555d7d1e80fddfd1ff82f233
Wolfero <ferorock44@gmail.com>
2022-06-01 12:23:07 +0100

    ⚰ removing dead code

--- pages/proyects/Proyects.jsx
+++ pages/proyects/Proyects.jsx
@@ -5,8 +5,6 @@ import proyectsLinks from './proyectsLinks';
 import styles from './Proyects.module.scss';
 
 function Proyects() {
-    const url = 'http://localhost:3000/proyects/ticktacktoe';
-
     return (
         <Box>
             {proyectsLinks.map(({ name, path }) => (

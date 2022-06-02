6bb200176e278d91b7f8288505edd54f73e4599f
Wolfero <ferorock44@gmail.com>
2022-06-02 12:52:04 +0100

    ♻ Refactoring TickTackToe

--- pages/proyects/ticktacktoe/TickTackToe.jsx
+++ pages/proyects/ticktacktoe/TickTackToe.jsx
@@ -1,82 +1,91 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, useColorMode } from "@chakra-ui/react";
 import { useCallback, useMemo, useState } from "react";
 
-import styles from './TickTackToe.module.scss';
+import styles from "./TickTackToe.module.scss";
 
 const DEFAULT_BOARD = [...Array(9)];
+const winningCases = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 
-const getWinner = squares => [
-    [0, 1, 2], [3, 4, 5],
-    [6, 7, 8], [0, 3, 6],
-    [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-].map(line =>
-    line.map(number => squares[number]))
-    .find(([a, b, c]) => a === b && a === c)?.[0];
+const getWinner = (squares) =>
+    winningCases
+        .map((line) => line.map((number) => squares[number]))
+        .find(([a, b, c]) => a === b && a === c)?.[0];
 
 const Game = () => {
     const [squares, setSquares] = useState(DEFAULT_BOARD);
-    const getNextPlayer = squares => squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
-    const nextPlayer = useMemo(() =>
-        getNextPlayer(squares),
-        [squares]);
-    const winner = useMemo(() =>
-        getWinner(squares),
-        [squares]);
-    const onRestartClick = useCallback(() =>
-        setSquares(DEFAULT_BOARD),
+    const winner = useMemo(() => getWinner(squares), [squares]);
+
+    const firstPLayer = "X";
+    const secondPlayer = "O";
+    const getNextPlayer = (squares) =>
+        squares.filter(Boolean).length % 2 === 0 ? firstPLayer : secondPlayer;
+    const nextPlayer = useMemo(() => getNextPlayer(squares), [squares]);
+
+    const onRestartClick = useCallback(
+        () => setSquares(DEFAULT_BOARD),
         [setSquares]
     );
+
     const onSquareClick = useCallback(
-        index => winner === undefined && squares[index] === undefined
-            ? setSquares([
-                ...squares.slice(0, index),
-                nextPlayer,
-                ...squares.slice(index + 1)
-            ])
-            : undefined,
+        (index) =>
+            winner === undefined && squares[index] === undefined
+                ? updateSquares(index)
+                : undefined,
         [nextPlayer, setSquares, squares, winner]
     );
 
+    function updateSquares(index) {
+        setSquares([
+            ...squares.slice(0, index),
+            nextPlayer,
+            ...squares.slice(index + 1),
+        ]);
+    }
+
+    const { colorMode } = useColorMode();
+    const isDark = colorMode === "dark" ? styles.DarkMode : "";
+    const restartButtonClass = [styles.Restart, isDark].join(" ");
+
     return (
-        <Box className={styles.Game} >
+        <Box className={styles.Game}>
+            <Heading className={styles.NextPLayer}>
+                {winner
+                    ? `Winner: ${winner}`
+                    : squares.every(Boolean)
+                    ? "Draw"
+                    : `Next player: ${nextPlayer}`}
+            </Heading>
+            <button className={restartButtonClass} onClick={onRestartClick}>
+                Restart
+            </button>
             <Board
                 {...{
-                    onRestartClick,
                     onSquareClick,
-                    nextPlayer,
                     squares,
-                    winner
+                    isDark,
                 }}
             />
         </Box>
     );
-}
+};
 
-export default Game;
+const Board = ({ onSquareClick, squares, isDark }) => {
+    const squareButtonClass = [styles.Square, isDark].join(" ");
 
-const Board = ({
-    nextPlayer,
-    onRestartClick,
-    onSquareClick,
-    squares,
-    winner,
-    ...props
-}) => (
-    <Box className={styles.GameBoard} {...props}>
-        <Heading className={styles.Status}>
-            {winner ? `Winner: ${winner}`
-                : squares.every(Boolean)
-                    ? "Draw"
-                    : `Next player: ${nextPlayer}`}
-        </Heading>
-        <button className={styles.Restart} onClick={onRestartClick}>
-            Restart
-        </button>
-        <Box className={styles.Squares}>
+    return (
+        <Box className={styles.Board}>
             {squares.map((square, index) => (
                 <button
-                    className={styles.Square}
+                    className={squareButtonClass}
                     key={index}
                     onClick={() => onSquareClick(index)}
                 >
@@ -84,6 +93,7 @@ const Board = ({
                 </button>
             ))}
         </Box>
-    </Box>
-);
+    );
+};
 
+export default Game;

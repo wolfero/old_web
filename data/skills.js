15a06880b6b52fe35f65e56fe9efbe4571d137e5
Wolfero <ferorock44@gmail.com>
2022-06-10 16:59:05 +0100

    ✨ add skills data

---
+++ data/skills.js
@@ -0,0 +1,26 @@
+export const skillsTags = [
+    {
+        type: "html",
+        title: "HTML"
+    },
+    {
+        type: "css",
+        title: "CSS"
+    },
+    {
+        type: "es6",
+        title: "Javascript (ES6+)"
+    },
+    {
+        type: "sass",
+        title: "SASS"
+    },
+    {
+        type: "react",
+        title: "REACT"
+    },
+    {
+        type: "git",
+        title: "GIT"
+    }
+]

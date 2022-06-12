00b27c122c3e46717e3ddf0b8cd7d91bf7bf3be9
Wolfero <ferorock44@gmail.com>
2022-06-12 19:49:17 +0100

    🚚 moved projectLinks to data

---
+++ data/projectsLinks.js
@@ -0,0 +1,8 @@
+export const projectsLinks = [
+    {
+        name: "Tick Tack Toe",
+        webLink: "/projects/ticktacktoe",
+        githubLink: "https://github.com/wolfero/web/tree/master/pages/projects/ticktacktoe",
+        image:"/assets/projects/ticktacktoe.jpg"
+    },
+];

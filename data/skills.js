1c714b75347997211df1dba8dfbf6cf7fc65037f
Wolfero <ferorock44@gmail.com>
2022-06-21 19:15:23 +0100

    ♻ update skills data

--- data/skills.js
+++ data/skills.js
@@ -2,31 +2,25 @@ export const skillsTags = [
     {
         type: "html",
         title: "HTML",
-        color: "red"
     },
     {
         type: "css",
         title: "CSS",
-        color: "cyan"
     },
     {
         type: "es6",
         title: "JavaScript",
-        color: "yellow"
     },
     {
         type: "sass",
         title: "Sass",
-        color: "pink"
     },
     {
         type: "react",
         title: "React",
-        color: "cyan"
     },
     {
         type: "git",
         title: "Git",
-        color: "orange",
     }
 ]

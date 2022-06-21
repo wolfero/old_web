36b0b4a5f6312ca0bf780397c977c0d29245edf6
Wolfero <ferorock44@gmail.com>
2022-06-21 10:15:23 +0100

    👽️ Add fetch to coingecko's API

--- src/views/projects/CryptoPrices/CryptoPrices.jsx
+++ src/views/projects/CryptoPrices/CryptoPrices.jsx
@@ -1,11 +1,31 @@
 import { Heading } from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { TableCoins } from "./components/TableCoins/TableCoins";
 
 import styles from "./CryptoPrices.module.scss";
 
-export const CryptoPrices=()=>{
+export const CryptoPrices = () => {
+    const [coins, setCoins] = useState([]);
+
+    const getData = async () => {
+        const response = await axios.get(
+            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d"
+        );
+        setCoins(response.data);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
-        <Heading textAlign={"center"}>🚧 In development</Heading>
+        <>
+            <Heading textAlign={"center"}>🚧 In development</Heading>
+            <TableCoins coins={coins} />
+        </>
     );
-}
+};
 
 export default CryptoPrices;

6db114eb2e6aed51c5a1102af7ccae40d8e83f5c
Wolfero <ferorock44@gmail.com>
2022-06-22 02:30:20 +0100

    🗃️ update projectsLinks

--- data/projectsLinks.js
+++ data/projectsLinks.js
@@ -1,26 +1,30 @@
 export const projectsLinks = [
     {
         name: "My Website",
+        description: "",
         webLink: "/",
         githubLink: "https://github.com/wolfero/web",
         image: ""
     },
     {
         name: "Tick Tack Toe",
+        description: "",
         webLink: "/projects/ticktacktoe",
         githubLink: "https://github.com/wolfero/web/tree/master/src/views/projects/TickTackToe",
         image: "/assets/projects/ticktacktoe.jpg"
     },
     {
         name: "Trello Clone",
+        description: "",
         webLink: "/projects/trello-clone",
         githubLink: "https://github.com/wolfero/web/tree/master/src/views/projects/TrelloClone",
         image: ""
     },
     {
         name: "Crypto Prices",
+        description: "In this exercise, I have practiced the basic react hooks, such as useState and ussEffect, also to make request to a third party api with axios.",
         webLink: "/projects/crypto-prices",
         githubLink: "https://github.com/wolfero/web/tree/master/src/views/projects/CryptoPrices",
-        image: ""
+        image: "/assets/projects/cryptoPrices.jpg"
     },
 ];

5582d55c78aee496a347609e9273bac1add46aeb
Wolfero <ferorock44@gmail.com>
2022-06-27 16:56:29 +0100

    🏗️ migrate ColoringPrice component to TS

--- src/views/projects/CryptoPrices/components/ColoringPrice/ColoringPrice.tsx
+++ src/views/projects/CryptoPrices/components/ColoringPrice/ColoringPrice.tsx
@@ -1,8 +1,10 @@
 import { Box, Td } from "@chakra-ui/react";
+import { FC } from "react";
 
+import { ColoringPriceProps } from "../../model/ColoringPrice/coloringPrice";
 import styles from "./ColoringPrice.module.scss";
 
-const ColoringPrice = ({ price }) => {
+const ColoringPrice: FC<ColoringPriceProps> = ({ price }) => {
     if (price < 0) {
         return (
             <Td textAlign={"center"}>

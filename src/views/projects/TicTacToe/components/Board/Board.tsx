8bb20d2a636a23bfd41dd0a686b25bbea7542470
Wolfero <ferorock44@gmail.com>
2022-06-27 16:12:49 +0100

    🏗️ migrate Board component to TS

---
+++ src/views/projects/TicTacToe/components/Board/Board.tsx
@@ -0,0 +1,23 @@
+import { Box } from "@chakra-ui/react";
+
+import styles from "./Board.module.scss";
+import Square from "../Square/Square";
+import { BoardProps } from "../../models/Board/board";
+
+const Board: React.FC<BoardProps> = ({ onSquareClick, squares, isDark }) => {
+    const squareButtonClass = [styles.Square, isDark].join(" ");
+    return (
+        <Box className={styles.Board}>
+            {squares.map((square, index: number) => (
+                <Square
+                    square={square}
+                    key={index}
+                    className={squareButtonClass}
+                    onClick={() => onSquareClick(index)}
+                />
+            ))}
+        </Box>
+    );
+};
+
+export default Board;

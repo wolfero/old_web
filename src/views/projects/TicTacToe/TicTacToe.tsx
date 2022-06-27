6c4378dfee2c616a36e24456a4cb7d095d815322
Wolfero <ferorock44@gmail.com>
2022-06-27 16:20:33 +0100

    🐛Fix bug in project tictactoe

--- src/views/projects/TicTacToe/TicTacToe.tsx
+++ src/views/projects/TicTacToe/TicTacToe.tsx
@@ -1,35 +1,56 @@
 import { Box, Heading, useColorMode } from "@chakra-ui/react";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useState } from "react";
 
-import Square from "./components/Square/Square";
+import Board from "./components/Board/Board";
+import { Value } from "./models/Board/board";
 import styles from "./TicTacToe.module.scss";
 
-const DEFAULT_BOARD: number[] = [...Array(9)];
-const winningCases = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
+function calculateWinner(squares: Value[]) {
+    const winningCombinations = [
+        // horizontal
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
 
-const getWinner = (squares: number[]) =>
-    winningCases
-        .map((line) => line.map((number) => squares[number]))
-        .find(([a, b, c]) => a === b && a === c)?.[0];
+        //vertical
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
 
+        //Cruz
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let line = 0; line < winningCombinations.length; line++) {
+        const [lef, c, r] = winningCombinations[line];
+        const leftEqualsCenter = squares[lef] && squares[lef] === squares[c];
+        const leftEqualsRight = squares[lef] === squares[r];
+        if (leftEqualsCenter && leftEqualsRight) {
+            return squares[lef];
+        }
+    }
+
+    return null;
+}
+
+const firstPLayer = "X";
+const secondPlayer = "O";
+function getNextPlayer(squares: Value[]) {
+    return squares.filter(Boolean).length % 2 === 0
+        ? firstPLayer
+        : secondPlayer;
+}
+
+function isFilled(squares: Value[], nextPlayer: "X" | "O") {
+    return squares.every(Boolean) ? "Draw" : `Next player: ${nextPlayer}`;
+}
+
+const DEFAULT_BOARD = Array<Value>(9).fill(null);
 export const Game = () => {
     const [squares, setSquares] = useState(DEFAULT_BOARD);
-    const winner = useMemo(() => getWinner(squares), [squares]);
-
-    const firstPLayer = "X";
-    const secondPlayer = "O";
-    const getNextPlayer = (squares: number[]) =>
-        squares.filter(Boolean).length % 2 === 0 ? firstPLayer : secondPlayer;
-    const nextPlayer = useMemo(() => getNextPlayer(squares), [squares]);
+    const winner = calculateWinner(squares);
+    const nextPlayer = getNextPlayer(squares);
 
     const onRestartClick = useCallback(
         () => setSquares(DEFAULT_BOARD),
@@ -37,29 +58,28 @@ export const Game = () => {
     );
 
     const onSquareClick = useCallback(
-        (index) =>
-            winner === undefined && squares[index] === undefined
-                ? setSquares([
-                      ...squares.slice(0, index),
-                      nextPlayer,
-                      ...squares.slice(index + 1),
-                  ])
-                : undefined,
-        [nextPlayer, setSquares, squares, winner]
+        (index: number) => {
+            const validation = !winner && !squares[index];
+            if (validation) {
+                const result = setSquares([
+                    ...squares.slice(0, index),
+                    nextPlayer,
+                    ...squares.slice(index + 1),
+                ]);
+                return result;
+            }
+        },
+        [nextPlayer, setSquares, squares]
     );
 
     const { colorMode } = useColorMode();
-    const isDark = colorMode === "dark" ? styles.DarkMode : "";
+    const isDark = colorMode === "dark" ? styles.DarkMode : styles.LightMode;
     const restartButtonClass = [styles.Restart, isDark].join(" ");
 
     return (
         <Box className={styles.Game} px={"20"} py={"5"}>
             <Heading className={styles.NextPLayer}>
-                {winner
-                    ? `Winner: ${winner}`
-                    : squares.every(Boolean)
-                    ? "Draw"
-                    : `Next player: ${nextPlayer}`}
+                {winner ? `Winner: ${winner}` : isFilled(squares, nextPlayer)}
             </Heading>
             <button className={restartButtonClass} onClick={onRestartClick}>
                 Restart

882c931b5e5aefb2499798b9abb8235d3ebeb0ae
Wolfero <ferorock44@gmail.com>
2022-06-30 00:01:35 +0100

    ✨Add Savvily project

--- data/projectsLinks.ts
+++ data/projectsLinks.ts
@@ -7,11 +7,26 @@ export const projectsLinks = [
         image: ""
     },
     {
+        name: "Savvily",
+        description: "I collaborated in the creation of Savvily.es website. The web is made with Hugo, JS, CSS and HTML",
+        webLink: "https://savvily.es/",
+        githubLink: "",
+        image: "/assets/projects/Savvily.jpg"
+    },
+    {
         name: "Tic Tac Toe",
         description: "This is an exercise in which I practice the hooks of useMemo, useCallback, useState and React components.",
         webLink: "/projects/tictactoe",
         githubLink: "https://github.com/wolfero/web/tree/master/src/views/projects/TicTacToe",
         image: "/assets/projects/tictactoe.jpg"
+    },
+
+    {
+        name: "Crypto Prices",
+        description: "Here I have practiced the basic react hooks, such as useState and ussEffect, also make requests to an api with axios.",
+        webLink: "/projects/crypto-prices",
+        githubLink: "https://github.com/wolfero/web/tree/master/src/views/projects/CryptoPrices",
+        image: "/assets/projects/cryptoPrices.jpg"
     },
     {
         name: "Trello Clone",
@@ -20,12 +35,4 @@ export const projectsLinks = [
         githubLink: "https://github.com/wolfero/web/tree/master/src/views/projects/TrelloClone",
         image: ""
     },
-    {
-        name: "Crypto Prices",
-        description: "In this exercise, I have practiced the basic react hooks, such as useState and ussEffect, also to make request to a third party api with axios.",
-        webLink: "/projects/crypto-prices",
-        githubLink: "https://github.com/wolfero/web/tree/master/src/views/projects/CryptoPrices",
-        image: "/assets/projects/cryptoPrices.jpg"
-    },
-
 ];

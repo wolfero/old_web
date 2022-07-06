bdbccadd7640e0e1b5942856089286ff2555f7e9
Wolfero <ferorock44@gmail.com>
2022-07-06 14:40:17 +0100

    🔧 Added firebase data

---
+++ data/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: 'AIzaSyChPCaKOiHJyU0ZCnI1vAJMyZ1jJP755-0',
+	authDomain: 'wolfero-trello-board-clone.firebaseapp.com',
+	projectId: 'wolfero-trello-board-clone',
+	storageBucket: 'wolfero-trello-board-clone.appspot.com',
+	messagingSenderId: '531568263446',
+	appId: '1:531568263446:web:1f2087186aced3d510d037',
+};

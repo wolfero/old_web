1c128742915cc6e9cd2d051357a7009242ce0fed
Wolfero <ferorock44@gmail.com>
2022-08-18 13:40:02 +0100

    🎨Improving card title editable

--- src/views/Projects/TrelloClone/components/CardTitle/CardTitle.tsx
+++ src/views/Projects/TrelloClone/components/CardTitle/CardTitle.tsx
@@ -1,6 +1,7 @@
-import { Heading } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Heading, Input } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import { DraggableProvided } from 'react-beautiful-dnd';
+import { BsThreeDots, BsTrashFill } from 'react-icons/bs';
 
 import styles from './CardTitle.module.scss';
 
@@ -10,15 +11,67 @@ type CardTitleProps = {
 };
 
 const CardTitle = ({ title, provided }: CardTitleProps) => {
+	const [open, setOpen] = useState(false);
+	// const [openOption, setOpenOption] = useState(false);
+	const [newTitle, setNewTitle] = useState(title);
+
+	const handleOnBlur = () => {
+		setOpen((prev) => !prev);
+	};
+
 	return (
-		<Heading
-			as={'h2'}
-			size={'lg'}
-			className={styles.TitleList}
-			{...provided.dragHandleProps}
-		>
-			{title}
-		</Heading>
+		<>
+			{open ? (
+				<Input
+					type={'text'}
+					value={newTitle}
+					onChange={(e) => setNewTitle(e.target.value)}
+					onBlur={handleOnBlur}
+					onKeyPress={(e) => {
+						if (e.key === 'Enter') handleOnBlur();
+					}}
+					autoFocus
+				/>
+			) : (
+				<Box className={styles.Title}>
+					<Heading
+						as={'h2'}
+						size={'lg'}
+						className={styles.Text}
+						{...provided.dragHandleProps}
+						onClick={() => setOpen((prev) => !prev)}
+					>
+						{newTitle}
+					</Heading>
+					<button
+						className={styles.Button}
+						// onClick={() => setOpenOption((prev) => !prev)}
+						onClick={() => console.log('deleted card')} //TODO DELETE CARD
+					>
+						{/* <BsThreeDots /> */}
+						<BsTrashFill />
+					</button>
+					{/* {openOption && (
+						//ClickOutComponent => https://www.npmjs.com/package/react-onclickout
+						<ClickOutComponent
+							onClickOut={(e) => {
+								setOpenOption((prev) => !prev);
+							}}
+						>
+							<ul>
+								<li
+									onClick={() => {
+										setOpenOption((curr) => !curr);
+									}}
+								>
+									Delete Card
+								</li>
+							</ul>
+						</ClickOutComponent>
+					)} */}
+				</Box>
+			)}
+		</>
 	);
 };
 

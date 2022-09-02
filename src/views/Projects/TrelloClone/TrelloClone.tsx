d8a11236ecd6b4eda074a55c1694feed18db8d22
Wolfero <ferorock44@gmail.com>
2022-09-02 10:20:14 +0100

    🔥 remove header

--- src/views/Projects/TrelloClone/TrelloClone.tsx
+++ src/views/Projects/TrelloClone/TrelloClone.tsx
@@ -8,9 +8,6 @@ import styles from './TrelloClone.module.scss';
 export const TrelloClone = () => {
 	return (
 		<Box className={styles.Container}>
-			<Heading textAlign={'center'} p={'2rem'}>
-				🚧 In development
-			</Heading>
 			<Heading as={'h1'} className={styles.Navbar}>
 				<BsBookmarkFill className={styles.BookMark} />
 				Trello Board Clone

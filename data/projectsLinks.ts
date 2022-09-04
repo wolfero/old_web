171c5edb07d26370c0f30956066c1238688028d4
Wolfero <ferorock44@gmail.com>
2022-09-04 10:01:57 +0100

    🚚 organize assets

--- data/projectsLinks.ts
+++ data/projectsLinks.ts
@@ -11,14 +11,14 @@ export const projectsLinks = [
         description: "I collaborated in the creation of Savvily.es website. The web is made with Hugo, JS, CSS and HTML",
         webLink: "https://savvily.es/",
         githubLink: "",
-        image: "/assets/projects/Savvily.jpg"
+        image: "/assets/projects/Savvily/Savvily.jpg"
     },
     {
         name: "Tic Tac Toe",
         description: "This is an exercise in which I practice the hooks of useMemo, useCallback, useState and React components.",
         webLink: "/projects/tictactoe",
         githubLink: "https://github.com/wolfero/web/tree/master/src/views/Projects/TicTacToe",
-        image: "/assets/projects/tictactoe.jpg"
+        image: "/assets/projects/TicTacToe/tictactoe.jpg"
     },
 
     {
@@ -26,14 +26,14 @@ export const projectsLinks = [
         description: "Here I have practiced the basic react hooks, such as useState and ussEffect, also make requests to an api with axios.",
         webLink: "/projects/crypto-prices",
         githubLink: "https://github.com/wolfero/web/tree/master/src/views/Projects/CryptoPrices",
-        image: "/assets/projects/cryptoPrices.jpg"
+        image: "/assets/projects/CryptoPrices/cryptoPrices.jpg"
     },
     {
         name: "Trello Clone",
         description: "In this project I try to create a trello clone with its basic functions: create cards and tasks, reorganize and edit them.",
         webLink: "/projects/trello-clone",
         githubLink: "https://github.com/wolfero/web/tree/master/src/views/Projects/TrelloClone",
-        image: "/assets/projects/trelloClone.jpg"
+        image: "/assets/projects/TrelloClone/trelloClone.jpg"
     },
     {
         name: "Rock, Paper, Scissors Game",

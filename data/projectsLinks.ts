fe45a52cc8a3e4c43aacc8796ea9933bbb1afb80
Wolfero <ferorock44@gmail.com>
2022-09-06 10:43:44 +0100

    🍱 added asset and description RPS_Game

--- data/projectsLinks.ts
+++ data/projectsLinks.ts
@@ -1,45 +1,52 @@
 export const projectsLinks = [
-    {
-        name: "This Website",
-        description: "The website is made with NextJs and JavaScript. For styling I use Chakra UI and Sass to customize components.",
-        webLink: "/",
-        githubLink: "https://github.com/wolfero/web",
-        image: ""
-    },
-    {
-        name: "Savvily",
-        description: "I collaborated in the creation of Savvily.es website. The web is made with Hugo, JS, CSS and HTML",
-        webLink: "https://savvily.es/",
-        githubLink: "",
-        image: "/assets/projects/Savvily/Savvily.jpg"
-    },
-    {
-        name: "Tic Tac Toe",
-        description: "This is an exercise in which I practice the hooks of useMemo, useCallback, useState and React components.",
-        webLink: "/projects/tictactoe",
-        githubLink: "https://github.com/wolfero/web/tree/master/src/views/Projects/TicTacToe",
-        image: "/assets/projects/TicTacToe/tictactoe.jpg"
-    },
+	{
+		name: 'This Website',
+		description:
+			'The website is made with NextJs and JavaScript. For styling I use Chakra UI and Sass to customize components.',
+		webLink: '/',
+		githubLink: 'https://github.com/wolfero/web',
+		image: '',
+	},
+	{
+		name: 'Savvily',
+		description:
+			'I collaborated in the creation of Savvily.es website. The web is made with Hugo, JS, CSS and HTML',
+		webLink: 'https://savvily.es/',
+		githubLink: '',
+		image: '/assets/projects/Savvily/Savvily.jpg',
+	},
+	{
+		name: 'Tic Tac Toe',
+		description:
+			'This is an exercise in which I practice the hooks of useMemo, useCallback, useState and React components.',
+		webLink: '/projects/tictactoe',
+		githubLink: 'https://github.com/wolfero/web/tree/master/src/views/Projects/TicTacToe',
+		image: '/assets/projects/TicTacToe/tictactoe.jpg',
+	},
 
-    {
-        name: "Crypto Prices",
-        description: "Here I have practiced the basic react hooks, such as useState and ussEffect, also make requests to an api with axios.",
-        webLink: "/projects/crypto-prices",
-        githubLink: "https://github.com/wolfero/web/tree/master/src/views/Projects/CryptoPrices",
-        image: "/assets/projects/CryptoPrices/cryptoPrices.jpg"
-    },
-    {
-        name: "Trello Clone",
-        description: "In this project I try to create a trello clone with its basic functions: create cards and tasks, reorganize and edit them.",
-        webLink: "/projects/trello-clone",
-        githubLink: "https://github.com/wolfero/web/tree/master/src/views/Projects/TrelloClone",
-        image: "/assets/projects/TrelloClone/trelloClone.jpg"
-    },
-    {
-        name: "Rock, Paper, Scissors Game",
-        description: "🚧 In development",
-        webLink: "/projects/rock-paper-scissors-game",
-        githubLink: "https://github.com/wolfero/web/tree/master/src/views/Projects/RockPaperScissorsGame",
-        image: ""
-    },
+	{
+		name: 'Crypto Prices',
+		description:
+			'Here I have practiced the basic react hooks, such as useState and ussEffect, also make requests to an api with axios.',
+		webLink: '/projects/crypto-prices',
+		githubLink: 'https://github.com/wolfero/web/tree/master/src/views/Projects/CryptoPrices',
+		image: '/assets/projects/CryptoPrices/cryptoPrices.jpg',
+	},
+	{
+		name: 'Trello Clone',
+		description:
+			'In this project I try to create a trello clone with its basic functions: create cards and tasks, reorganize and edit them.',
+		webLink: '/projects/trello-clone',
+		githubLink: 'https://github.com/wolfero/web/tree/master/src/views/Projects/TrelloClone',
+		image: '/assets/projects/TrelloClone/trelloClone.jpg',
+	},
+	{
+		name: 'Rock, Paper, Scissors',
+		description:
+			'Here I decided to make the rock, paper, scissors game, to reinforce my basic knowledge of react.',
+		webLink: '/projects/rock-paper-scissors-game',
+		githubLink:
+			'https://github.com/wolfero/web/tree/master/src/views/Projects/RockPaperScissorsGame',
+		image: '/assets/projects/RockPaperScissorsGame/Rock_Paper_Scissors_Game.jpg',
+	},
 ];

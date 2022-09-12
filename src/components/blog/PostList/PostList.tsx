694acfd0c3564ef376db862ae6281a2d35c95678
Wolfero <ferorock44@gmail.com>
2022-09-12 10:31:38 +0100

    💄styling articles

--- src/components/blog/PostList/PostList.tsx
+++ src/components/blog/PostList/PostList.tsx
@@ -1,30 +1,23 @@
-import { Heading, Wrap, WrapItem, Box } from "@chakra-ui/react";
-import PostPreview from "../PostPreview/PostPreview";
-import { PostsProps } from "../../../model/Blog/posts";
-import styles from "./PostList.module.scss";
+import { Heading, WrapItem, Box } from '@chakra-ui/react';
+import PostPreview from '../PostPreview/PostPreview';
+import { PostsProps } from '../../../model/Blog/posts';
+import styles from './PostList.module.scss';
 
 const PostList = ({ posts }: PostsProps) => {
-    return (
-        <Box as={"article"} className={styles.Posts}>
-            <Heading as="h2" className={styles.Title}>
-                Articles
-            </Heading>
-            <Box as={"section"} className={styles.List}>
-                {posts.map((post) => (
-                    <WrapItem
-                        key={post.slug}
-                        width={{
-                            base: "100%",
-                            md: "45%",
-                            lg: "30%",
-                        }}
-                    >
-                        <PostPreview key={post.slug} post={post} />
-                    </WrapItem>
-                ))}
-            </Box>
-        </Box>
-    );
+	return (
+		<Box as={'article'} className={styles.Posts}>
+			<Heading as="h2" className={styles.Title}>
+				Articles
+			</Heading>
+			<Box as={'ul'} className={styles.List}>
+				{posts.map((post) => (
+					<WrapItem key={post.slug} className={styles.Element}>
+						<PostPreview key={post.slug} post={post} />
+					</WrapItem>
+				))}
+			</Box>
+		</Box>
+	);
 };
 
 export default PostList;
